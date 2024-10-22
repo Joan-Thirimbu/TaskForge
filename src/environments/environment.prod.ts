cf726db1cbb84d03506dc6eae7ac96483dd1212e
Joan-Thirimbu <joanwangare3@gamil.com>
2024-10-22 12:02:29 +0300

    updated apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://joan-thirimbu.github.io/Task-Manager'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiUrl: 'https://joan-thirimbu.github.io/Task-Manager'
+  production: false
 };
 
 /*
